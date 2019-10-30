8ff5df318848c5d913dacc0a16147be29a7afe5f
Thomas Woinke <thomas@steckschwein,de>
2019-10-30 22:11:48 +0100

    bring via2 back

--- via.c
+++ via.c
@@ -12,7 +12,6 @@
 #include "glue.h"
 
 static uint8_t via1registers[16];
-static uint8_t via1pb_in;
 
 void
 via1_init()
@@ -23,33 +22,65 @@ via1_init()
 uint8_t
 via1_read(uint8_t reg)
 {
-	if (reg == 0) {
-		// PB
-		// 0 input  -> take input bit
-		// 1 output -> take output bit
-		return (via1pb_in & (via1registers[2] ^ 0xff)) |
-		  (via1registers[0] & via1registers[2]);
-		} else {
-		  switch (reg) {
-			case 4:
-			case 5:
-			case 8:
-			case 9:
-				// timer A and B: return random numbers for RND(0)
-				// XXX TODO: these should be real timers :)
-				return rand() & 0xff;
-			default:
-				return via1registers[reg];
-		  }
+	switch (reg) {
+		case 4:
+		case 5:
+		case 8:
+		case 9:
+			// timer A and B: return random numbers for RND(0)
+			// XXX TODO: these should be real timers :)
+			return rand() & 0xff;
+		default:
+			return via1registers[reg];
+	}
 }
-}
-
-
 
 void
 via1_write(uint8_t reg, uint8_t value)
 {
 	via1registers[reg] = value;
+	// TODO
+}
+
+//
+// VIA#2
+//
+// PA0 PS/2 DAT
+// PA1 PS/2 CLK
+// PA2 LCD backlight
+// PA3 NESJOY latch (for both joysticks)
+// PA4 NESJOY joy1 data
+// PA5 NESJOY joy1 CLK
+// PA6 NESJOY joy2 data
+// PA7 NESJOY joy2 CLK
+
+static uint8_t via2registers[16];
+static uint8_t via2pb_in;
+
+void
+via2_init()
+{
+}
+
+uint8_t
+via2_read(uint8_t reg)
+{
+	if (reg == 0) {
+		// PB
+		// 0 input  -> take input bit
+		// 1 output -> take output bit
+		return (via2pb_in & (via2registers[2] ^ 0xff)) |
+			(via2registers[0] & via2registers[2]);
+	} else {
+		return via2registers[reg];
+	}
+}
+
+void
+via2_write(uint8_t reg, uint8_t value)
+{
+	via2registers[reg] = value;
+
 	if (reg == 0) {
 		// PB
 	} else if (reg == 2) {
@@ -57,22 +88,20 @@ via1_write(uint8_t reg, uint8_t value)
 	}
 }
 
-
-
 uint8_t
-via1_pb_get_out()
+via2_pb_get_out()
 {
-	return via1registers[2] /* DDR  */ & via1registers[0]; /* PB */
+	return via2registers[2] /* DDR  */ & via2registers[0]; /* PB */
 }
 
 void
-via1_pb_set_in(uint8_t value)
+via2_pb_set_in(uint8_t value)
 {
-	via1pb_in = value;
+	via2pb_in = value;
 }
 
 void
-via1_sr_set(uint8_t value)
+via2_sr_set(uint8_t value)
 {
-	via1registers[10] = value;
+	via2registers[10] = value;
 }
