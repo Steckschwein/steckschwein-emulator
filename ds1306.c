7b7b1156cbf8b9c9c7c1dea16da210ca1fcd129e
mlauke <m.lauke@web.de>
2019-12-04 01:23:20 +0100

    +rtc behaviour, read time

--- ds1306.c
+++ ds1306.c
@@ -1,13 +1,21 @@
 #include <inttypes.h>
 #include <time.h>
 #include "ds1306.h"
+#include "glue.h"
 
-static struct tm* timestamp;
+static struct tm *timestamp;
+static bool auto_inc = false;
+static uint8_t nvram[96];
 
 void spi_rtc_select() {
 	time_t ts = time(NULL);
 	timestamp = localtime(&ts); //update timestamp with systime
 	printf("rtc select() %s\n", asctime(timestamp));
+	auto_inc = false;
+}
+
+uint8_t toBCD(uint8_t v) {
+	return (v / 10) << 4 | (v % 10);
 }
 
 uint8_t spi_rtc_handle(uint8_t inbyte) {
@@ -16,32 +24,46 @@ uint8_t spi_rtc_handle(uint8_t inbyte) {
 
 	static uint8_t addr = 0;
 
-	if (inbyte != 0x7) {
-		addr = inbyte & 0x0f; //adjust addr pointer
-	} else
-		switch (addr) {
-		case 0:
-			outbyte = timestamp->tm_sec;
-			break;
-		case 1:
-			outbyte = timestamp->tm_min;
-			break;
-		case 2:
-			outbyte = timestamp->tm_hour;
-			break;
-		case 3:
-			outbyte = timestamp->tm_wday;
-			break;
-		case 4:
-			outbyte = timestamp->tm_mday;
-			break;
-		case 5:
-			outbyte = timestamp->tm_year;
-			break;
+	if (!auto_inc) {
+		addr = inbyte;
+		auto_inc = true;
+	} else {
+		if (addr & 0x80) { //write
+			if (addr & 0x7f >= 0x20) { //nvram access
+				nvram[addr - 0x20] = inbyte;
+			}
+		} else { //read
+			if (addr >= 0x20) { //nvram access
+				outbyte = nvram[addr - 0x20];
+			} else
+				switch (addr) {
+				case RTC_SECONDS:
+					outbyte = toBCD(timestamp->tm_sec);
+					break;
+				case RTC_MINUTES:
+					outbyte = toBCD(timestamp->tm_min);
+					break;
+				case RTC_HOURS:
+					outbyte = toBCD(timestamp->tm_hour);
+					break;
+				case RTC_DAY_OF_WEEK:
+					outbyte = toBCD(timestamp->tm_wday);
+					break;
+				case RTC_DATE_OF_MONTH:
+					outbyte = toBCD(timestamp->tm_mday);
+					break;
+				case RTC_MONTH:
+					outbyte = toBCD(timestamp->tm_mon) + 1;
+					break;
+				case RTC_YEAR:
+					outbyte = toBCD(timestamp->tm_year - 100);
+					break;
+				}
 		}
-	addr++;
+		addr++;
+	}
 
-	printf("rtc %x %x\n",inbyte, outbyte);
+	printf("rtc %x %x\n", inbyte, outbyte);
 
 	return outbyte;
 }
