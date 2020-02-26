bacd373f03d4c940d8d19e4b265e7e3663a7a504
Thomas Woinke <thomas@steckschwein,de>
2020-02-26 21:15:42 +0100

    65c02 opcodes RMBx/SMBx

--- cpu/65c02.h
+++ cpu/65c02.h
@@ -73,7 +73,7 @@ static void phx() {
 
 static void plx() {
     x = pull8();
-   
+
     zerocalc(x);
     signcalc(x);
 }
@@ -84,14 +84,14 @@ static void phy() {
 
 static void ply() {
     y = pull8();
-  
+
     zerocalc(y);
     signcalc(y);
 }
 
 // *******************************************************************************************
 //
-//								TRB & TSB - Test and Change bits 
+//								TRB & TSB - Test and Change bits
 //
 // *******************************************************************************************
 
@@ -108,6 +108,88 @@ static void trb() {
     result = (uint16_t)a & value;  					// calculate A & memory
     zerocalc(result); 								// Set Z flag from this.
     result = value & (a ^ 0xFF); 					// Write back value read, A bits are clear.
+    putvalue(result);
+}
+
+static void rmb0() {
+    value = getvalue();
+    result = (uint16_t) value & 0b11111110;
+    putvalue(result);
+}
+static void rmb1() {
+    value = getvalue();
+    result = (uint16_t) value & 0b11111101;
+    putvalue(result);
+}
+static void rmb2() {
+    value = getvalue();
+    result = (uint16_t) value & 0b11111011;
+    putvalue(result);
+}
+static void rmb3() {
+    value = getvalue();
+    result = (uint16_t) value & 0b11110111;
+    putvalue(result);
+}
+static void rmb4() {
+    value = getvalue();
+    result = (uint16_t) value & 0b11101111;
+    putvalue(result);
+}
+static void rmb5() {
+    value = getvalue();
+    result = (uint16_t) value & 0b11011111;
+    putvalue(result);
+}
+static void rmb6() {
+    value = getvalue();
+    result = (uint16_t) value & 0b10111111;
+    putvalue(result);
+}
+static void rmb7() {
+    value = getvalue();
+    result = (uint16_t) value & 0b01111111;
+    putvalue(result);
+}
+
+static void smb0() {
+    value = getvalue();
+    result = (uint16_t) value ^ 0b00000001;
+    putvalue(result);
+}
+static void smb1() {
+    value = getvalue();
+    result = (uint16_t) value ^ 0b00000010;
+    putvalue(result);
+}
+static void smb2() {
+    value = getvalue();
+    result = (uint16_t) value ^ 0b00000100;
+    putvalue(result);
+}
+static void smb3() {
+    value = getvalue();
+    result = (uint16_t) value ^ 0b00001000;
+    putvalue(result);
+}
+static void smb4() {
+    value = getvalue();
+    result = (uint16_t) value ^ 0b00010000;
+    putvalue(result);
+}
+static void smb5() {
+    value = getvalue();
+    result = (uint16_t) value ^ 0b00100000;
+    putvalue(result);
+}
+static void smb6() {
+    value = getvalue();
+    result = (uint16_t) value ^ 0b01000000;
+    putvalue(result);
+}
+static void smb7() {
+    value = getvalue();
+    result = (uint16_t) value ^ 0b10000000;
     putvalue(result);
 }
 
