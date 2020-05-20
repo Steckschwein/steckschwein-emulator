2319d09ce7ef3d5a591465371b6a6da179f969e1
mlauke <m.lauke@web.de>
2020-05-20 21:40:39 +0200

    +clean code

--- sdcard.c
+++ sdcard.c
@@ -27,7 +27,29 @@ uint8_t block_response[2 //data token
 		+ 4 //busy wait
 ];
 
-//uint8_t block_write[BLOCK_SIZE];
+static uint8_t *p_data = NULL;
+static int data_length = 0;
+static int data_counter = 0;
+static uint8_t mblock = 0; //multi block counter
+
+int readBlock(uint32_t lba){
+	int res = fread(&block_response[2], sizeof(uint8_t), BLOCK_SIZE, sdcard_file);
+	if (!res) {
+		fprintf(stderr, "fread error block $%x, lba %x: %s\n", mblock, lba, strerror(errno));
+	}else{
+		if (res != BLOCK_SIZE) {
+			WARN("Warning: short read lba %x, bytes %d/%d'!\n", lba, res, BLOCK_SIZE);
+		}
+		p_data = block_response;
+		data_length = 2 + BLOCK_SIZE + 2 + 2;
+		data_counter = 0; //start over
+		block_response[0] = 0;
+		block_response[1] = 0xfe;
+		memset(&block_response[2 + BLOCK_SIZE + 2], 0xff, 4);
+	}
+	return res;
+}
+
 
 uint8_t spi_sdcard_handle(uint8_t inbyte) {
 
@@ -37,24 +59,19 @@ uint8_t spi_sdcard_handle(uint8_t inbyte) {
 	static uint8_t cmd[6];
 	static uint8_t last_cmd;
 
-	static uint8_t *response = NULL;
-	static int response_length = 0;
-	static int response_counter = 0;
-
-	static uint8_t mblock = 0; //multi block counter
 	uint8_t outbyte = 0xff;
 
-	if (response) {
-		outbyte = response[response_counter]; //read one byte from buffer
-		response[response_counter] = inbyte; // write one byte to buffer
-		response_counter++;
-		if (response_counter == response_length) {
+	if (p_data) {
+		outbyte = p_data[data_counter]; //read one byte from buffer
+		p_data[data_counter] = inbyte; // write one byte to buffer
+		data_counter++;
+		if (data_counter == data_length) {
 
 			if (last_cmd == 0x40 + 24) { //write block if it was requested
 				uint32_t lba = cmd[1] << 24 | cmd[2] << 16 | cmd[3] << 8 | cmd[4];
 				printf("Writing LBA %d block (%d)\n", lba + mblock, mblock);
-				for (int i = 0; i < response_length; ++i) {
-					DEBUG("%x ", response[i]);
+				for (int i = 0; i < data_length; ++i) {
+					DEBUG("%x ", p_data[i]);
 				}
 				int res = fwrite(&block_response[2], sizeof(uint8_t), BLOCK_SIZE, sdcard_file);
 				if (!res) {
@@ -65,29 +82,16 @@ uint8_t spi_sdcard_handle(uint8_t inbyte) {
 				}
 			}
 
-			response = NULL;
+			p_data = NULL;
 		}
 	} else if (cmd_receive_counter == 0 && inbyte == 0xff) {
-		if (!response) { // send response data
+		if (!p_data) { // send response data
 			if (last_cmd == 0x40 + 18) { //last captured cmd was read multiblock
 				uint32_t lba = cmd[1] << 24 | cmd[2] << 16 | cmd[3] << 8 | cmd[4];
 				DEBUG("Reading LBA %d multiblock (%d)\n", lba + mblock, mblock);
 
-				int res = fread(&block_response[2], sizeof(uint8_t), BLOCK_SIZE, sdcard_file);
-				if (!res) {
-					fprintf(stderr, "fread error block $%x, lba %x: %s\n", mblock, lba, strerror(errno));
-				}else {
-					if (res != BLOCK_SIZE) {
-						WARN("Warning: short read bytes %d/%d'!\n", res, BLOCK_SIZE);
-					}
-					block_response[0] = 0;
-					block_response[1] = 0xfe;
-					memset(&block_response[2 + BLOCK_SIZE + 2], 0xff, 4);
-					response = block_response;
-					response_length = 2 + BLOCK_SIZE + 2 + 2;
-					response_counter = 0; //start over
-					mblock++;
-				}
+				int res = readBlock(lba);
+				mblock++;
 			}
 		}
 	} else {
@@ -101,39 +105,39 @@ uint8_t spi_sdcard_handle(uint8_t inbyte) {
 				// CMD0: init SD card to SPI mode
 				static const uint8_t r[] = { 1 };
 				block_response[0] = 1;
-				response = block_response;
-				response_length = sizeof(r);
+				p_data = block_response;
+				data_length = sizeof(r);
 				break;
 			}
 			case 0x40 + 8: { //CMD8: SEND_IF_COND
 				static const uint8_t r[] = { 1, 0x00, 0x00, 0x01, 0xaa };
-				response_length = sizeof(r);
-				memcpy(block_response, r, response_length);
-				response = block_response;
+				data_length = sizeof(r);
+				memcpy(block_response, r, data_length);
+				p_data = block_response;
 				break;
 			}
 			case 0x40 + 12: { //CMD12 - end transmission, e.g. on multi block read
 				mblock = 0;
 				static const uint8_t r[] = { 0 }; //1, 0x00, 0x00, 0x01, 0xaa };
-				response_length = sizeof(r);
-				memcpy(block_response, r, response_length);
-				response = block_response;
+				data_length = sizeof(r);
+				memcpy(block_response, r, data_length);
+				p_data = block_response;
 				break;
 			}
 			case 0x40 + 41: {
 				static const uint8_t r[] = { 0 };
-				response_length = sizeof(r);
-				memcpy(block_response, r, response_length);
-				response = block_response;
+				data_length = sizeof(r);
+				memcpy(block_response, r, data_length);
+				p_data = block_response;
 				break;
 			}
 			case 0x40 + 16: {
 				// set block size
 				DEBUG("set block size = 0x%04x\n", cmd[2] << 8 | cmd[3]);
 				static const uint8_t r[] = { 1 };
-				response_length = sizeof(r);
-				memcpy(block_response, r, response_length);
-				response = block_response;
+				data_length = sizeof(r);
+				memcpy(block_response, r, data_length);
+				p_data = block_response;
 				break;
 			}
 			case 0x40 + 17: { // read block
@@ -145,16 +149,7 @@ uint8_t spi_sdcard_handle(uint8_t inbyte) {
 				if(fs){
 					fprintf(stderr, "error fseek %s\n", strerror(errno));
 				}else{
-					int res = fread(&block_response[2 + res], sizeof(uint8_t), BLOCK_SIZE - res, sdcard_file);
-					if (!res) {
-						fprintf(stderr, "fread error block lba %x: %s\n", lba, strerror(errno));
-					}else{
-						if (res != BLOCK_SIZE) {
-							WARN("Warning: short read lba %x, bytes %d/%d'!\n", lba, res, BLOCK_SIZE);
-						}
-						response = block_response;
-						response_length = 2 + BLOCK_SIZE + 2;
-					}
+					int res = readBlock(lba);
 				}
 				break;
 			}
@@ -178,36 +173,36 @@ uint8_t spi_sdcard_handle(uint8_t inbyte) {
 					block_response[0] = 0;
 					block_response[1] = 0xfe;
 					memset(&block_response[2 + BLOCK_SIZE + 2], 0xff, 4);
-					response = block_response;
-					response_length = 2 + BLOCK_SIZE + 2 + 2;
+					p_data = block_response;
+					data_length = 2 + BLOCK_SIZE + 2 + 2;
 					mblock = 0;
 				}
 				break;
 			}
 			case 0x40 + 55: { //CMD55 APP_CMD
 				static const uint8_t r[] = { 1 };
-				response_length = sizeof(r);
-				memcpy(block_response, r, response_length);
-				response = block_response;
+				data_length = sizeof(r);
+				memcpy(block_response, r, data_length);
+				p_data = block_response;
 				break;
 			}
 			case 0x40 + 58: { //READ_OCR
 				static const uint8_t r[] = { 0, 0, 0, 0 };
-				response_length = sizeof(r);
-				memcpy(block_response, r, response_length);
-				response = block_response;
+				data_length = sizeof(r);
+				memcpy(block_response, r, data_length);
+				p_data = block_response;
 				break;
 			}
 			default: {
 				static const uint8_t r[] = { 0 };
-				response_length = sizeof(r);
-				memcpy(block_response, r, response_length);
-				response = block_response;
+				data_length = sizeof(r);
+				memcpy(block_response, r, data_length);
+				p_data = block_response;
 				break;
 			}
 			}
 			last_cmd = cmd[0];
-			response_counter = 0;
+			data_counter = 0;
 		}
 	}
 
